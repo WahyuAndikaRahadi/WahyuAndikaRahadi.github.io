dcdda3920666f52f380fc218c6d1e46e3d58cc71
WahyuAndikaRahadi <wahyuandikarahadi19@gmail.com>
2024-01-30 13:35:34 +0700

    penambahan

---
+++ game dino/main.js
@@ -0,0 +1,35 @@
+const dino = document.getElementById("dino");
+const rock = document.getElementById("rock");
+const score = document.getElementById("score");
+
+function jump() {
+  dino.classList.add("jump-animation");
+  setTimeout(() =>
+    dino.classList.remove("jump-animation"), 500);
+}
+
+document.addEventListener('keydown', (event) => {
+  if (!dino.classList.contains('jump-animation')) {
+    jump();
+  }
+})
+
+setInterval(() => {
+  const dinoTop = parseInt(window.getComputedStyle(dino)
+    .getPropertyValue('top'));
+  const rockLeft = parseInt(window.getComputedStyle(rock)
+    .getPropertyValue('left'));
+  score.innerText++;
+
+  if (rockLeft < 0) {
+    rock.style.display = 'none';
+  } else {
+    rock.style.display = ''
+  }
+
+  if (rockLeft < 50 && rockLeft > 0 && dinoTop > 150) {
+   alert("Kamu mendapatkan score sebesar: " + score.innerText +
+      "\n\nCoba Lagi?");
+    location.reload();
+  }
+}, 50);
