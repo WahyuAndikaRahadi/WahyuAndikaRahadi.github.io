04419a9b46a45df35aae4884733890a156f645b4
WahyuAndikaRahadi <wahyuandikarahadi19@gmail.com>
2024-03-04 17:56:24 +0700

    penambahan repo

---
+++ login hand/js/main.js
@@ -0,0 +1,10 @@
+function playAudio() {
+    var audio = document.getElementById("myAudio");
+    var playButton = document.getElementById("playButton");
+  
+  
+    audio.play();
+  
+  
+    playButton.style.display = "none";
+  }
