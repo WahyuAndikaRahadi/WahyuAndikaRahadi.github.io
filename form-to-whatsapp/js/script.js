7c5be3e4c0a8e1a55bbf80ecb7ffef987fc42a20
WahyuAndikaRahadi <wahyuandikarahadi19@gmail.com>
2024-04-30 19:33:06 +0700

    penambahan repo

---
+++ form-to-whatsapp/js/script.js
@@ -0,0 +1,8 @@
+const nama = document.getElementById('nama')
+const pesan = document.getElementById('pesan')
+
+function sendMessageToWhatsapp() {
+    const urlToWhatsapp = `https://wa.me/6283816927804?text=Halo, nama saya ${nama.value}, ${pesan.value}`;
+
+    window.open(urlToWhatsapp, "_blank");
+  }
